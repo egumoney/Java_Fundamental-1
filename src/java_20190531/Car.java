245b6e34f8188ce0325d5d2bc4e8f50074811875
syh1011 <syh@hbilab.org>
2019-05-31 13:29:01 +0900

    2019-05-31

---
+++ src/java_20190531/Car.java
@@ -0,0 +1,10 @@
+package java_20190531;
+
+public class Car {
+	private String modelNumber;
+	String color;
+	protected int doorCount;
+	public double price;
+	
+	
+}
---
+++ src/java_20190531/Overloading.java
@@ -0,0 +1,53 @@
+package java_20190531;
+
+public class Overloading {
+	
+	
+	public void print(int i){
+		System.out.println(i);
+	}
+	
+	public void print(String s){
+		System.out.println(s);
+	}
+	
+	public void print(double d){
+		System.out.println(d);
+	}
+	
+	public void printInt(int i){
+		System.out.println(i);
+		
+	}
+	
+	public void search(String id){
+		
+	}
+	
+	public void search(String f, String e){
+		
+	}
+	
+	public void search(String f,String m, String e){
+		
+	}
+	
+	public void search(String zipcde ,int flag, String address){
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
