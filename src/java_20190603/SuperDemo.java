beb9ce57a9924a4c86e7fd7d647beb41d2b92894
syh1011 <syh@hbilab.org>
2019-06-03 18:23:22 +0900

    2019-6-3-v4

---
+++ src/java_20190603/SuperDemo.java
@@ -0,0 +1,21 @@
+package java_20190603;
+import java.lang.*;
+class A {
+	String name;
+	A(){}
+	A(String name){
+		super();
+		this.name = name;
+	}
+}
+
+class B extends A{
+	B(){
+		super();
+	}
+}
+
+
+public class SuperDemo {
+	
+}
