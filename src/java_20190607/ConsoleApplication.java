c094be20c24c3e1bba6f1ad9c95c9b32b908a97a
syh1011 <syh@hbilab.org>
2019-06-07 18:11:37 +0900

    2019-06-07-v3

--- src/java_20190607/ConsoleApplication.java
+++ src/java_20190607/ConsoleApplication.java
@@ -45,25 +45,26 @@ public class ConsoleApplication {
 		String id = console("아이디 : ");
 		String name = console("이름 : ");
 		Member m = new Member(name,id);
+		
 		list.add(m);
 		
+		common();
+		
+	}
+	private void common() throws IOException {
 		for(Member m1 :list){
-			//System.out.println("아이디 : "+m1.getId()+", 이름 : "+m1.getName());
-			/*System.out.printf("아이디 : %s, 이름 : %s%n",
-					m1.getId(),m1.getName());*/
 			System.out.println(m1);
 		}
 		print();
 		String message = console("선택하세요 : ");
 		execute(message);
-		
 	}
 	//1. 수정할 아이디를 입력받는다.
 	//2. ArrayList 에서 수정할 아이디를 찾아서 있으면 수정할 이름을 입력받고 ArrayList에서 수정하고, 
-	//없으면 수정할 아디디가 없습니다. 메세지 출력한다.
+	//없으면 수정할 아이디가 없습니다. 메세지 출력한다.
 	//3. ArrayList에 있는 모든 고객을 출력한다.
-	//5. print() 함수호출
-	//6. 선택하세요 : 메세지 출력후 키보드 입력받을 준비
+	//4. print() 함수호출
+	//5. 선택하세요 : 메세지 출력후 키보드 입력받을 준비
 	public void update() throws IOException{
 		String updateId = console("수정할 아이디 : ");
 		boolean isExisted = false;
@@ -78,9 +79,7 @@ public class ConsoleApplication {
 			for(int i = 0; i<list.size();i++){
 				Member m1 = list.get(i);
 				if(updateId.equals(m1.getId())){
-					//list.remove(i);
-					//list.add(new Member(updateName,updateId));
-					
+					m1.setName(updateName);
 					break;
 				}
 			}
@@ -88,23 +87,60 @@ public class ConsoleApplication {
 			System.out.println("수정할 아디디가 없습니다");
 		}
 		
-		for(Member m1 :list){
-			//System.out.println("아이디 : "+m1.getId()+", 이름 : "+m1.getName());
-			/*System.out.printf("아이디 : %s, 이름 : %s%n",
-					m1.getId(),m1.getName());*/
-			System.out.println(m1);
-		}
-		print();
-		String message = console("선택하세요 : ");
-		execute(message);
+		common();
 		
 	}
+	//1. 삭제할 아이디를 입력받는다.
+	//2. ArrayList 에서 삭제할 아이디를 찾아서 있으면  ArrayList에서 삭제하고, 
+	//없으면 삭제할 아이디가 없습니다. 메세지 출력한다.
+	//3. ArrayList에 있는 모든 고객을 출력한다.
+	//4. print() 함수호출
+	//5. 선택하세요 : 메세지 출력후 키보드 입력받을 준비
 	public void delete() throws IOException{
+		String updateId = console("삭제할 아이디 : ");
+		boolean isExisted = false;
+		for(int i=0;i<list.size();i++){
+			Member m = list.get(i);
+			if(updateId.equals(m.getId())){
+				isExisted = true;
+				list.remove(i);
+				break;
+			}
+		}
+		if(!isExisted){
+			System.out.println("삭제할 아디디가 없습니다");
+		}
 		
+		common();
 	}
+	
+	//1. 검색할 아이디를 입력받는다.
+	//2. ArrayList 에서 검색할 아이디를 찾아서 있으면  ArrayList에서 출력하고, 
+	//없으면 검색할 아이디가 없습니다. 메세지 출력한다.
+	//3. ArrayList에 있는 모든 고객을 출력한다.
+	//4. print() 함수호출
+	//5. 선택하세요 : 메세지 출력후 키보드 입력받을 준비
+	
 	public void search() throws IOException{
+		String searchId = console("검색할 아이디 : ");
+		System.out.println("***************검색결과*****************");
+		boolean isExisted = false;
+		for(int i=0;i<list.size();i++){
+			Member m = list.get(i);
+			if(searchId.equals(m.getId())){
+				isExisted = true;
+				System.out.println(m);
+				break;
+			}
+		}
+		if(!isExisted){
+			System.out.println("검색된 결과가 없습니다");
+		}
+		System.out.println("**************************************");
 		
+		common();
 	}
+	
 	
 	public static void main(String[] args) throws IOException {
 		ConsoleApplication c = new ConsoleApplication();
