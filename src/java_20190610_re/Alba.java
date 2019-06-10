dc267e98ffef1edfe6e368dc16085bc1377caf3d
syh1011 <syh@hbilab.org>
2019-06-10 21:08:16 +0900

    2019-6-10-v3

---
+++ src/java_20190610_re/Alba.java
@@ -0,0 +1,29 @@
+package java_20190610_re;
+
+import java.util.ArrayList;
+
+public class Alba extends Parent{
+	String address;
+	//ctl + shirt + o => ÀÚµ¿ import
+	ArrayList<Alba> list = new ArrayList<Alba>();
+	
+	public Alba(String name, String cphone, String address) {
+		super();
+		this.name = name;
+		this.cphone = cphone;
+		this.address = address;
+	}
+	public void insert(){
+		list.add(new Alba(name,cphone,address));
+	}
+	
+	public void delete(int i){
+		list.remove(i);
+	}
+	
+}
+
+
+
+
+
---
+++ src/java_20190610_re/Parent.java
@@ -0,0 +1,8 @@
+package java_20190610_re;
+
+public abstract class Parent {
+	String name;
+	String cphone;
+	public abstract void insert();
+	public abstract void delete(int i);
+}
